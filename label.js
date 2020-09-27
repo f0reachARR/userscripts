3388a0f18af31eb9c57b5f8ff155878137c3369d
f0reachARR <f0reach@f0reach.me>
2020-09-27 15:39:21 +0900

    Add label to title and list

--- label.js
+++ label.js
@@ -15,7 +15,11 @@ function getCourseLabelMap() {
 
 function setCourseLabel(courseId, text) {
   const labels = getCourseLabelMap();
-  labels[courseId] = text;
+  if (text.length > 0) {
+    labels[courseId] = text;
+  } else if (typeof labels[courseId] === 'string') {
+    delete labels[courseId];
+  }
   localStorage.setItem(COURSE_LABEL_STORAGE_KEY, JSON.stringify(labels));
 }
 
@@ -61,15 +65,31 @@ function showLabelOnSidebarCourse() {
 
   const labels = getCourseLabelMap();
   for (const course of Object.keys(labels)) {
+    // マイコース ツリー, マイコース リスト
     cssTag.sheet.insertRule(
-      `p.tree_item[data-node-type="20"][data-node-key="${course}"]::after {
+      `p.tree_item[data-node-type="20"][data-node-key="${course}"]::after,
+        .block_course_list a[href*="/course/view.php?id=${course}"]::after {
           margin-left: .3em;
           content: "${labels[course]}";
           border: .3px solid #333;
           padding: 0 .2em;
           border-radius: .2em;
+          text-decoration: none;
         }`,
     );
+
+    if (location.href.includes(`/course/view.php?id=${course}`)) {
+      // コースページタイトル
+      cssTag.sheet.insertRule(
+        `.page-header-headings h1::after {
+            margin-left: .3em;
+            content: "${labels[course]}";
+            border: .3px solid #333;
+            padding: 0 .2em;
+            border-radius: .2em;
+          }`,
+      );
+    }
   }
 }
 
